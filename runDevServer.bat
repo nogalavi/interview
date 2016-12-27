4035ff826e97777c43810db2fd27e7ea0345a7b6
Yehuda Sabag <yehuda.sabag@biocatch.com>
2016-12-27 14:13:49 +0200

    First commit with simple website

---
+++ runDevServer.bat
@@ -0,0 +1,1 @@
+node ..\node_modules\http-server\bin\http-server -p 8000 --cors --ssl --cert 20245029-localhost.cert --key 20245029-localhost.key
---
+++ runDevServer.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+node node_modules/http-server/bin/http-server -p 8000
